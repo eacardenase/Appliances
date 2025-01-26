bafb547d947efbd51d278fcd694be133280280f7
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-25 21:38:06 -0500

    feat: add deadly init method

---
+++ BNRWallSafe.h
@@ -0,0 +1,16 @@
+//
+//  BNRWallSafe.h
+//  Appliances
+//
+//  Created by Edwin Cardenas on 25/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BNRWallSafe : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BNRWallSafe.m
@@ -0,0 +1,18 @@
+//
+//  BNRWallSafe.m
+//  Appliances
+//
+//  Created by Edwin Cardenas on 25/01/25.
+//
+
+#import "BNRWallSafe.h"
+
+@implementation BNRWallSafe
+
+- (instancetype)init
+{
+    [NSException raise:@"BNRWallSafeInitialization"
+                format:@"Use initWithSecretCode:, not init"];
+}
+
+@end
